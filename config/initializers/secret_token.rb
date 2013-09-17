4d5e3f85e5b0be34744cd6368323c1ea537014ef
Joel AZEMAR <joel.azemar@gmail.com>
2013-09-17 17:13:47 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoughHew::Application.config.secret_token = '300c23776cc58beeefe10ae96f04423242ba1b89156491d25d970ec2956c768d0018b5e19c244c2d3f125b8c99418fdf58d514825b65794180487d603287559e'
